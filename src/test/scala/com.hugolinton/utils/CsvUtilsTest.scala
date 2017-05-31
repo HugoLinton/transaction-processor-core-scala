94e6e10a7f7fb2fa03d6e4b9591b3237c59d54d1
HugoLinton <hugolinton@live.co.uk>
2017-05-31 21:16:04 +0100

    Added empty Test

---
+++ src/test/scala/com.hugolinton/utils/CsvUtilsTest.scala
@@ -0,0 +1,23 @@
+package com.hugolinton.utils
+
+//import org.junit.runner.RunWith
+//import org.scalatest.junit.JUnitRunner
+//import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+/**
+  * Created by hugol on 31/05/2017.
+  */
+//@RunWith(classOf[JUnitRunner])
+object CsvUtilsTest {
+
+  //Wasn't sure if I should use Maven to pull in the usual unit test dependencies
+
+//  "Valid CSV Filepath string " should " return a list Transaction case classes" in {
+//    val filePath = getClass.getResource("/transactionsreduced.txt").getPath
+//
+//    val transactions = CsvUtils.getTransactions(filePath)
+//
+//    transactions.size should be (15)
+//  }
+
+}
